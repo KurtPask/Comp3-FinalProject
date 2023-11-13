b345db1a4bdc25376b44653877ef594aadfd9fef
KurtPask <kurtpasque@gmail.com>
2023-11-13 09:57:17 -0800

    updated batch file

--- HPC_batch.sh
+++ HPC_batch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH --array=1-99
+#SBATCH --array=1-3
 #SBATCH --output=batch_print_outputs/out_%a.txt
 
 . /etc/profile
 module load lang/python/3.8.11
 pip install protobuf==3.20.0
+pip install tensorflow==2.10.0
+pip install h5py==3.9.0
+pip install numpy==1.24.3
+pip install keras=2.10.0
 
-output_file="$HOME/Comp3-FinalProject/print_outputs/out_${SLURM_ARRAY_TASK_ID}.txt"
-
-python $HOME/Comp3-FinalProject/HPC_script.py $SLURM_ARRAY_TASK_ID > $output_file
+python $HOME/Comp3-FinalProject/HPC_script.py $SLURM_ARRAY_TASK_ID 
